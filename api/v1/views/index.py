8f60c783e53748ef3f6dcdec9346e68807b6f0fc
Silvycr <silvia_alinacr@live.com>
2023-09-23 13:56:02 -0700

    Modifying the imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Script that import app_views"""
-import app_views from api.v1.views
+from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
-def api_tatus():
+
+def api_status():
     """a function to return api status"""
     return jsonify({"status": "OK"})
