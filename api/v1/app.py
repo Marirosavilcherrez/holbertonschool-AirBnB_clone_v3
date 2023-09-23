cacbbd5009f3c90e62936968987db13c777b3d26
Marirosa <arqmvilcherrez@gmail.com>
2023-09-23 12:30:06 -0700

    Add Api files to the project

---
+++ api/v1/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web"""
+from flask import Flask, render_template
+from models import storage
+from api.v1.views import app_view
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_route(err):
+    "Close the SQLAlchemy Session"
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True )
---
+++ api/v1/views/index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Script that import app_views"""
+import app_views from api.v1.views
+
+
+
+
