2ca05ac3fe3a723f4778760031f9d1d65a517f5b
Silvycr <silvia_alinacr@live.com>
2023-09-23 13:29:46 -0700

    Modifying the module status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,14 @@
 import app_views from api.v1.views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def api_status():
     """a function to return api status"""
-    return jsonify({"status": "OK"})
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
