87d373b6aa7aea38f1748cc1d14e1058ed8636e1
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-09-23 18:45:35 -0500

    Fix conflicts to the merge

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,6 @@
 """initialization file for views module"""
 from flask import Blueprint
 
-<<<<<<< HEAD
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-=======
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
->>>>>>> c8f399eaa9ff65a9ffdfa05c49c3b8b12308bb8c
