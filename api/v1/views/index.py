40877a7b9bea597db6e1fe60e1849bf41a0d6aec
Silvycr <silvia_alinacr@live.com>
2023-09-23 13:21:26 -0700

    Modifying the method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 import app_views from api.v1.views
 from flask import jsonify
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """a function to return api status"""
     return jsonify({"status": "OK"})
