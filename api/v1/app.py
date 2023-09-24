62b564d92d81efb2d70c5e11aa6692e9d142370f
Silvycr <silvia_alinacr@live.com>
2023-09-23 17:38:35 -0700

    Modifiying the file app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,14 @@
 """Script that starts a Flask web"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models import storage
-import os
 from flask_cors import CORS
+from models import storage
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
@@ -18,7 +19,6 @@ def close_route(error):
 
 
 if __name__ == '__main__':
-
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
-    cors = CORS(app, origins="0.0.0.0")
-    app.run(host='localhost', port=port, threaded=True)
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
+    app.run(host=host, port=port, threaded=True)
