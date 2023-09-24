82fc7d3aeb3b0833328030859d9337d44b9382ad
Silvycr <silvia_alinacr@live.com>
2023-09-23 17:37:42 -0700

    Create an endpoint that retrieves the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,27 @@
 """Script that import app_views"""
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def api_status():
     """a function to return api status"""
-    data = {
-        "status": "OK"
-    }
+    return jsonify({"status": "OK"})
 
-    resp = jsonify(data)
-    resp.status_code = 200
-    return resp
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def obj_stats():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
