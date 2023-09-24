26741b7dad5852d859120b0c6d536c2d388caf7b
Marirosa <arqmvilcherrez@gmail.com>
2023-09-24 14:19:59 -0700

    Checked if the user_id in the request JSON exists in the database before creating a new Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,23 +3,26 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models.place import Place
+from models.city import City
+from models.user import User
 from models import storage
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
-def get_places():
+def get_places(city_id):
     """retrives the list of places"""
-    all_places = storage.all(Place).values()
-    list_places = []
-    for place in all_places:
-        list_places.append(place.to_dict())
-    return jsonify(list_places)
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    list_places = [place.to_dict() for place in city.places]:
+        return jsonify(list_places)
 
 
 @app_views.route('/places/<place_id>',
                  methods=['GET'], strict_slashes=False)
 def get_place(place_id):
-    """retrives users of objects"""
+    """retrives a place of objects"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -41,22 +44,28 @@ def delete_place(place_id):
 
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
-def post_place():
+def post_place(city_id):
     """create a place"""
     if not request.get_json():
         abort(400, description="Not a JSON")
 
-    if 'user_id' not in request.get_json():
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    data = request.get_json()
+    if 'user_id' not in data:
         abort(400, description="Missing user_id")
 
-    if 'user_id' not in request.get_json(User):
-        abort(400)
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
 
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
 
     data = request.get_json()
-    instance = Place(**data)
+    instance = Place(city_id=city_id, **data)
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
 
